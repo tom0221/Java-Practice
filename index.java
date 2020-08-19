f27e12a19fd03993eee7b955e7b8da7233023e3e
Tatsuro Muto <tomuto0221@gmail.com>
2020-08-19 09:57:09 +0900

    変数基本系

--- index.java
+++ index.java
@@ -2,8 +2,53 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		// Hello Java文字列を出力
 		System.out.println("Hello Java!");
+
+		// 数値の17を出力
+    	System.out.println( 17 );
+
+	    // 5に3を足した値を出力
+	    System.out.println( 5 + 3 );
+
+	    // 「5 + 3」を文字列として出力
+	    System.out.println("5 + 3");
+
+	    // 12を3で割った値を出力
+	    System.out.println( 12 / 3);
+
+	    // 3に6を掛けた値を出力
+	    System.out.println( 3 * 6 );
+
+	    // 8を3で割った時の余りを出力
+	    System.out.println(8 % 3);
+
+	    // "こんにちは"と"世界"を連結して出力
+	    System.out.println("こんにちは" + "世界");
+
+	    // "38"と"19"を連結して出力
+	    System.out.println("38" + "19");
+
+	    // 38と19を足して出力
+	    System.out.println( 38 + 19 );
+
+	    // int型の変数numberを定義
+	    int number;
+
+	    // 変数numberに3を代入
+	    number = 3;
+
+	    // 変数numberを出力
+	    System.out.println(number);
+
+	    // String型の変数nameを定義
+	    String name;
+
+	    // 変数nameに"Wanko"を代入
+	    name = "Wanko";
+
+	    // 変数nameを出力
+	    System.out.println(name);
 	}
 
 }
