44351e2c3121c3c183046a59362bf4836ae25ec9
Tatsuro Muto <tomuto0221@gmail.com>
2020-08-29 21:57:54 +0900

    出力追加

--- Person.java
+++ Person.java
@@ -7,5 +7,7 @@ class Person {
 	// 文字列「こんにちは」と出力する
 		System.out.println("こんにちは");
 		System.out.println("Java太郎です");
+		System.out.println("Javaの学習中です");
+		System.out.println("明日の天気は晴れです");
 	}
 }
