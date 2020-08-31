2222f573ebc4f9cca2351212f0a7e8c533b0fd46
Tatsuro Muto <tomuto0221@gmail.com>
2020-09-01 00:50:53 +0900

    インスタンスメソッドまで

--- Main4.java
+++ Main4.java
@@ -2,10 +2,24 @@
 class Main {
 	public static void main(String[] args) {
 		Person person1 = new Person();
+		// Personクラス1 helloメソッドを呼び出し。
+		person1.hello();
+
+		// person1のインスタンスフィールドnameに名前を定義
+		person1.name = "Kate Jones";
+		// インスタンスフィールドnameの値を出力
+		System.out.println(person1.name);
+
+
+
+		// 2番目のperson2のhelloメソッドを呼び出す。
 		Person person2 = new Person();
+		// Personクラス2 helloメソッドを呼び出し。
+		person2.hello();
 
-
-		// Personクラス helloメソッドを呼び出し。
-		person.hello();
+		// person2のインスタンスフィールドnameに名前を定義
+		person2.name = "John Christopher Smith";
+		// インスタンスフィールドnameの値を出力
+		System.out.println(person2.name);
 	}
 }
--- Person.java
+++ Person.java
@@ -1,8 +1,5 @@
 class Person {
 
-
-
-
 	public void hello() {
 	// 文字列「こんにちは」と出力する
 		System.out.println("こんにちは");
