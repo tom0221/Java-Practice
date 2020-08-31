e7d4bbbdbe5b344509554f8db0e79df2bb0f72d4
Tatsuro Muto <tomuto0221@gmail.com>
2020-08-31 23:12:51 +0900

    例文修正

--- Person.java
+++ Person.java
@@ -6,8 +6,5 @@ class Person {
 	public void hello() {
 	// 文字列「こんにちは」と出力する
 		System.out.println("こんにちは");
-		System.out.println("Java太郎です");
-		System.out.println("Javaの学習中です");
-		// System.out.println("明日の天気は晴れです");
 	}
 }
