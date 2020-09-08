d37c92c752e9254972e7bb69b6bb64aed3e7c7d1
Tatsuro Muto <tomuto0221@gmail.com>
2020-09-08 16:01:23 +0900

    演習(走行距離を測る)

--- Bicycle.java
+++ Bicycle.java
@@ -2,15 +2,18 @@
 class Bicycle{
 	private String name;
 	private String color;
+	private int distance = 0;
 
 	// インスタンスの生成時にフィールドに値をセットできるよう、コンストラクタを用意
 	Bicycle(String name, String color){
 		this.name = name;
 		this.color = color;
+		this.distance = distance;
 	}
 
 	public void printData(){
 		System.out.println("名前：" + this.name);
 		System.out.println("色：" + this.color);
+		System.out.println("走行距離：" + this.distance);
 	}
 }
